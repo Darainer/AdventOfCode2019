c52b9b9ec8a472f3d23300f648652c91d7d84ab0
Darainer <ryanjmatthews@gmail.com>
2019-12-25 14:09:03 +0100

    passing 5a. IntCode is now pretty ugly and needs some refactoring
    Signed-off-by: Darainer <ryanjmatthews@gmail.com>

--- Day5_Sunny_with_Asteroids/Day5_run_TEST_diagnostic.py
+++ Day5_Sunny_with_Asteroids/Day5_run_TEST_diagnostic.py
@@ -1,9 +1,20 @@
 from IntCode import IntCode
 
-simpleInput_opcode3 = [3,0,4,0,99]
-simpleOutputFile = "output_simple_programcodes.txt"
-Intcoder = IntCode(simpleInput_opcode3, simpleOutputFile)
-output = Intcoder.run_Intcode_with_input_output(43)
-print(output)
+# simpleInput_opcode3 = [3,0,4,0,99]
+# simpleOutputFile = "output_simple_programcodes.txt"
+# Intcoder = IntCode(simpleInput_opcode3, simpleOutputFile)
+# output = Intcoder.run_Intcode_with_input_output(43)
+# print(output)
 
-#Intcoder.write_output()
+# simpleInput_opcode2 = [1002,4,3,4,33]
+# simpleOutputFile = "output_simple_programcodes.txt"
+# Intcoder = IntCode(simpleInput_opcode2, simpleOutputFile)
+# output = Intcoder.run_Intcode_with_input_output(43)
+# print(output)
+
+simpleInput_opcode2 = "Day_5_input.txt"
+simpleOutputFile = "output_programcodes.txt"
+Intcoder = IntCode(simpleInput_opcode2, simpleOutputFile)
+Intcoder.run_Intcode_with_input(1)
+
+Intcoder.write_output()
--- IntCode.py
+++ IntCode.py
@@ -1,4 +1,13 @@
 # intcode computer
+
+def split_int_to_list(int_number: int) -> list:
+    digit_list = []
+    while int_number > 0:
+        digit_list.append(int_number % 10)
+        int_number //= 10
+    digit_list.reverse()
+    return digit_list
+
 class IntCode:
     def __init__(self, input_code, output_file):
         if type(input_code) is str:
@@ -7,15 +16,13 @@ class IntCode:
             self.program_codes = input_code
         self.original_codes = list(self.program_codes) # make an initial copy
         self.output_file = output_file
-
         self.input = -1
         self.output = -1
 
 
-    def run_Intcode_with_input_output(self,input_int: int)->int:
+    def run_Intcode_with_input(self,input_int: int):
         self.input = input_int
         self.compute_program()
-        return self.output
 
     def read_program_txt(self, input_file):
         with open(input_file, 'r') as file:
@@ -43,42 +50,62 @@ class IntCode:
     def compute_program(self) -> bool:
         idx = 0
         while idx <= len(self.program_codes):
-            if self.program_codes[idx] == 1:
-                self.intcode_operation_1(idx)
+            program_code_list = split_int_to_list(self.program_codes[idx])
+            if program_code_list[-1] == 1:
+                self.intcode_operation_1(idx,program_code_list)
                 idx = idx + 4
-            elif self.program_codes[idx] == 2:
-                self.intcode_operation_2(idx)
+            elif program_code_list[-1] == 2:
+                self.intcode_operation_2(idx,program_code_list)
                 idx = idx + 4
-            elif self.program_codes[idx] == 3:
+            elif program_code_list[-1] == 3:
                 self.intcode_operation_3(idx, self.input)
                 idx = idx + 2
-            elif self.program_codes[idx] == 4:
-                self.intcode_operation_4(idx)
+            elif program_code_list[-1] == 4:
+                self.intcode_operation_4(idx, program_code_list)
                 idx = idx + 2
-            elif self.program_codes[idx] == 99:  # program finish
+            elif program_code_list[-1] and program_code_list[-2] == 9:  # code 99 program finish
                 return True
             else:
                 return False
 
-    def intcode_operation_1(self, index) -> bool:  #addition
-        idx_1 = self.program_codes[index+1]
-        idx_2 = self.program_codes[index+2]
+    def get_op_arguments(self, index, program_code_list: list) -> list:
+        #can be just a single opcode if others are all leading zeros
+        arg_2 = 0
+        opmode_param1 = opmode_param2 = 0
+        if len(program_code_list)>=3:
+            opmode_param1 = program_code_list[-3]
+        if len(program_code_list)>=4:
+            opmode_param2 = program_code_list[-4]
+        if opmode_param1 == 0:
+            idx_m1 = self.program_codes[index + 1]
+            arg_1 = self.program_codes[idx_m1]
+        elif opmode_param1 ==1:
+            arg_1 = self.program_codes[index + 1]
+        if opmode_param2 == 0 and (program_code_list[-1] != 4):
+            idx_m2 = self.program_codes[index + 2]
+            arg_2 = self.program_codes[idx_m2]
+        elif opmode_param2 ==1:
+            arg_2 = self.program_codes[index + 2]
+        return [arg_1,arg_2]
+
+    def intcode_operation_1(self, index, program_code_list: list) -> bool:  #addition
+        [arg1, arg2] =self.get_op_arguments(index, program_code_list)
         idx_result = self.program_codes[index + 3]
-        self.program_codes[idx_result] = self.program_codes[idx_1] + self.program_codes[idx_2]
+        self.program_codes[idx_result] = arg1 + arg2
 
-    def intcode_operation_2(self, index)-> bool:  #multiplication
-        idx_m1 = self.program_codes[index + 1]
-        idx_m2 = self.program_codes[index + 2]
+    def intcode_operation_2(self, index, program_code_list: list)-> bool:  #multiplication
+        [arg1, arg2] = self.get_op_arguments(index, program_code_list)
         idx_mresult = self.program_codes[index + 3]
-        self.program_codes[idx_mresult] = self.program_codes[idx_m1] * self.program_codes[idx_m2]
+        self.program_codes[idx_mresult] = arg1 * arg2
 
-    def intcode_operation_3(self,index, input :int):  # save value
-        idx_1 = self.program_codes[index + 1]
-        self.program_codes[idx_1] = input
+    def intcode_operation_3(self, index, input: int):  # save value
+        idx_result = self.program_codes[index + 1]
+        self.program_codes[idx_result] = input
 
-    def intcode_operation_4(self,index):            # retrieve value
-        idx_1 = self.program_codes[index + 1]
-        self.output = self.program_codes[idx_1]
+    def intcode_operation_4(self, index,program_code_list: list):            # retrieve value
+        [arg1, arg2] = self.get_op_arguments(index, program_code_list)
+        self.output = arg1
+        print("diagnostic result", self.output)
 
     def write_output(self):
         f = open(self.output_file, 'w')
